cf07c4ce6fcce6ae90114860dbc0c6a69d35260e
Mercedes Magnelli <mechamagnelli@gmail.com>
2021-05-19 12:20:58 -0300

    agrego cosas con LocalDate

--- src/main/java/APICLima/Clima.java
+++ src/main/java/APICLima/Clima.java
@@ -3,21 +3,27 @@ package APICLima;
 
 import exceptions.MuchasConsultasException;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class Clima {
   //la haría un singleton así podría ser conocida en varios lugares
 
   APIClima unaAPIClima;
+  LocalDate ultimaConsulta;
   private static final Clima INSTANCE = new Clima();
 
   public static Clima instance() {
     return INSTANCE;
   }
-
+  //aca tengo la duda de si el getWeather me setea ya la fecha, sino tndría que meter el chequeo ahi,
+  //pero creería ue está hasta acá
   public Map<String, Object> climaSegunAPISeleccionada() {
-    unaAPIClima.controlarCantidadMaximaDeConsultas();
+    ultimaConsulta = LocalDate.now();
+      unaAPIClima.controlarCantidadMaximaDeConsultas();
     return unaAPIClima.getWeather("Buenos Aires, Argentina").get(0);
   }
 
